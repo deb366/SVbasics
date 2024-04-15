20729853958f119889b48ef1b2b3185d5dc36ddf
deb366 <166632278+deb366@users.noreply.github.com>
2024-04-15 21:56:38 +0530

    Create Parameterised_Penc.sv
    This is a parameterised priority encoder

---
+++ Parameterised_Penc.sv
@@ -0,0 +1,19 @@
+module Parameterised_Penc #(
+  parameter N = 4,
+  localparam N1 = $clog2(N)
+)(
+  input logic [N-1:0] I_ENC,
+  output logic [N1-1:0] O_enc
+);
+  
+  always_comb begin 
+    O_enc = '0;
+    for (int i = 0; i < N; i++) begin
+      if (I_ENC[i]) begin
+        O_enc = i;
+        break; // Exit loop after finding the first set bit
+      end
+    end
+  end 
+  
+endmodule 
