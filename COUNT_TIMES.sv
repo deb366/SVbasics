47cde25a6cb7c9f67a045c830f066f67e2df7d68
deb366 <166632278+deb366@users.noreply.github.com>
2024-06-03 02:14:48 +0530

    Update COUNT_TIMES.sv
    WIP

--- COUNT_TIMES.sv
+++ COUNT_TIMES.sv
@@ -2,22 +2,25 @@
 //WIP
 
 
+// Code your design here
+
+
 module COUNT_TIMES #(parameter N =4) (input logic clk,rstn, output logic [$clog2(N)-1:0] O_COUNT);
   
   localparam N0 = $clog2(N);  // Number of bits to count up to N
   logic [N0-1:0] r_times;
-  logic r_times, w_next;
+  logic  w_next;
 
 always_ff @(posedge clk)begin 
   if(!rstn) O_COUNT <= 'h0;
   else begin 
     if(w_next) begin 
     	O_COUNT <= O_COUNT + 1;
-      	r_times < = 'h0;
+      	r_times <= 'h0;
     end 
     else begin 
       O_COUNT <= O_COUNT;
-      r_times <= r_times + 1;
+      //r_times <= r_times + 1;
     end 
   end 
 end 
