aabf281fedb642cb88f2c2cc42c253b41158ef35
deb366 <166632278+deb366@users.noreply.github.com>
2024-06-28 22:42:58 +0530

    Update COUNT_TIMES.sv
    Added the final code

--- COUNT_TIMES.sv
+++ COUNT_TIMES.sv
@@ -1,34 +1,36 @@
 // 57.	Design a parameterized (N is the max count) logic where each clock will generate the output like (0,1,2,2,3,3,3,4,4,4,4,…,N with N times)? 
-//WIP
-
-// Code your design here
 
 
 module COUNT_TIMES #(
-    parameter N = 4,            // Declare N here
+    parameter N = 32,           // Declare N here, I_TIMES <= 2^32 -1
     localparam N0 = $clog2(N)   // Then declare N0 using N
 ) (
     input logic clk, rstn,
-    output logic [N0-1:0] O_COUNT
+  input logic [N0-1:0] I_TIMES,
+  output logic [N0-1:0] O_COUNT,
+  output logic O_END
 ); // Number of bits to count up to N
   
   logic [N0-1:0] r_times;
   logic  w_next;
 
 always_ff @(posedge clk)begin 
-  if(!rstn) O_COUNT <= 'h0;
+  if(!rstn || O_END) begin O_COUNT <= 'h0; r_times <= 'h0; end 
   else begin 
     if(w_next) begin 
     	O_COUNT <= O_COUNT + 1;
-      	r_times <= 'h0;
+      	r_times <= 1;
     end 
     else begin 
       O_COUNT <= O_COUNT;
-      //r_times <= r_times + 1;
+      r_times <= r_times + 1;
     end 
   end 
 end 
 
-assign w_next = r_times == O_COUNT;
+  //assign w_next = (r_times == O_COUNT) && |O_COUNT && (r_times == (O_COUNT -1));
+  assign w_next = r_times == O_COUNT ;
+  assign O_END = (O_COUNT == I_TIMES) && (r_times == I_TIMES);
   
 endmodule
+
