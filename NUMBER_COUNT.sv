6c05c1d41c09f7f34505e11328ef142a54f7f600
deb366 <166632278+deb366@users.noreply.github.com>
2024-06-29 18:14:53 +0530

    Update NUMBER_COUNT.sv
    added the sort part

--- NUMBER_COUNT.sv
+++ NUMBER_COUNT.sv
@@ -31,17 +31,18 @@ The module will take 8bit input , and gives 32bit output the output is the total
 	  
 	endmodule 
 
-
+//++++++++++++++++++++++++++++++++++++++++++++++++//
 //The below code gives the output with sorted array 
-	module count_numbers(
-	  input logic clk,rstn,
-	  input logic [7:0] I_NUM,
-      input logic sortit,
-	  output logic [7:0] O_NUM,
-      output logic [7:0] O_CNT,
-      output logic [7:0] O_MAX_REQUEST_ID,
-      output logic [7:0] O_MAX_REQUEST_NUM
-	);
+//++++++++++++++++++++++++++++++++++++++++++++++++//
+module count_numbers(
+  input logic clk,rstn,
+  input logic [7:0] I_NUM,
+  input logic sortit,
+  output logic [7:0] O_NUM,
+  output logic [7:0] O_CNT,
+  output logic [7:0] O_MAX_REQUEST_ID,
+  output logic [7:0] O_MAX_REQUEST_NUM
+);
 	  
 	  //logic [255:0][7:0] r_ram;
       logic [7:0] r_ram[256];
@@ -50,27 +51,27 @@ The module will take 8bit input , and gives 32bit output the output is the total
       logic [7:0] tmp;
       logic [7:0] tmp_id;
 	  
-	  always_ff @(posedge clk)begin 
-	    if(!rstn)begin 
+  always_ff @(posedge clk)begin 
+	if(!rstn)begin 
           r_ram <= '{default:8'h0};
-	      r_inum <= '0;
-	    end
+	  r_inum <= '0;
+	end
 	    
-	    else begin 
-	      r_ram[I_NUM] <= r_ram[I_NUM] + 1'b1;
-	      r_inum <= I_NUM;      
-	    end    
-	  end
+	else begin 
+	  r_ram[I_NUM] <= r_ram[I_NUM] + 1'b1;
+	  r_inum <= I_NUM;      
+	end    
+  end
 	  
-	  integer i, j;
-      typedef struct packed {
+integer i, j;
+typedef struct packed {
         logic [7:0] id;
         logic [7:0] value;
-      } items_t;
+} items_t;
       
-      items_t items[256];
+items_t items[256];
 	  
-      always_comb begin
+always_comb begin
         if (sortit) begin
             // Bubble sort algorithm
           for(i = 0;i<256;i++) begin 
@@ -93,13 +94,13 @@ The module will take 8bit input , and gives 32bit output the output is the total
                 end
             end
         end
-    end
+end
       
-	assign O_NUM = r_inum;
-	assign O_CNT = r_ram[r_inum];
-    assign O_MAX_REQUEST_ID = items[0].id;
-    assign O_MAX_REQUEST_NUM = items[0].value;
+assign O_NUM = r_inum;
+assign O_CNT = r_ram[r_inum];
+assign O_MAX_REQUEST_ID = items[0].id;
+assign O_MAX_REQUEST_NUM = items[0].value;
       
       
 	  
-	endmodule 
+endmodule 
