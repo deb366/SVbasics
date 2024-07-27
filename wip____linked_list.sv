c8cf421ea15908f88bdda746d1805c7676e84c6f
deb366 <166632278+deb366@users.noreply.github.com>
2024-07-27 07:16:51 +0530

    Update wip____linked_list.sv

--- wip____linked_list.sv
+++ wip____linked_list.sv
@@ -20,26 +20,28 @@ module linked_list #(parameter DATA_WIDTH = 8, SIZE = 10)
     // Array to hold nodes, simulating the linked list
     //reg [DATA_WIDTH+31:0] list[SIZE-1:0];
     node_t alist[SIZE-1:0];
-    reg [31:0] head;
-  reg [31:0] free_ptr,new_node;  // Points to the first free index (node)
+    logic [31:0] head;
+  //logic [31:0] free_ptr;
+  logic [31:0] new_node;  // Points to the first free index (node)
 
     // Initialize the linked list
     always_ff @(posedge clk) begin
       if (!rstn) begin
             head <= -1;  // Indicates an empty list
-            free_ptr <= 0;  // Start of the free list
+            //free_ptr <= 0;  // Start of the free list
             new_node <= 0;
             for (int i = 0; i < SIZE; i++) begin
               alist[i].next = (i < (SIZE - 1)) ? i + 1 : -1;
             end
         end 
       else if (insert) begin
-            if (free_ptr != -1) begin
-                new_node <= free_ptr;  // Allocate a new node from the free list
-                free_ptr <= alist[new_node].next;  // Update free pointer
+        if (new_node != -1) begin
+                //new_node <= free_ptr;  // Allocate a new node from the free list
+                new_node <= alist[new_node].next;
+                //free_ptr <= alist[new_node].next;  // Update free pointer
                 alist[new_node].data <= data_in;  // Set new node data
                 alist[new_node].next <= head;  // New node points to the previous head
-                head = new_node;  // Head points to the new node
+                head <= new_node;  // Head points to the new node
             end
         end
     end
@@ -53,6 +55,7 @@ module linked_list #(parameter DATA_WIDTH = 8, SIZE = 10)
       end
   
 endmodule
+
 
 //+++++++++++++++++++ TB ++++++++++++++++++++//
 
